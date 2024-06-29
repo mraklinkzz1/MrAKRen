764d99554d2fdbd3b723c4521a9a2490856a9428
MrAK Techz <l20cs001@vvcoe.org>
2024-06-29 13:13:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -16,7 +16,7 @@ SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '22225617'))
 API_HASH = environ.get('API_HASH', 'ef16f7597376f1689663304c954e4493')
 BOT_TOKEN = environ.get('BOT_TOKEN', "7140749651:AAFz9dm5qtawaIhtwRz5crvO5CeNGckmBvY")
-
+PM_SEARCH = bool(environ.get('PM_SEARCH', False))
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
