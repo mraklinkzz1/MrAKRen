d8e9b848bec624240036787d2a5ae61eb430b6a6
MrAK Techz <l20cs001@vvcoe.org>
2024-06-29 12:54:18 +0530

    Update Premium.py

--- plugins/Premium.py
+++ plugins/Premium.py
@@ -147,7 +147,11 @@ async def plan(client, message):
     users = message.from_user.mention 
     btn = [[
 
-        InlineKeyboardButton("📲 ꜱᴇɴᴅ ᴘᴀʏᴍᴇɴᴛ ꜱᴄʀᴇᴇɴꜱʜᴏᴛ ʜᴇʀᴇ", url=USERNAME)],[InlineKeyboardButton("❌ ᴄʟᴏꜱᴇ ❌", callback_data="close_data")
+            InlineKeyboardButton("📲 ꜱᴇɴᴅ ᴘᴀʏᴍᴇɴᴛ ꜱᴄʀᴇᴇɴꜱʜᴏᴛ ʜᴇʀᴇ", url=USERNAME)
+        ],[
+            InlineKeyboardButton('💰 ɢᴇᴛ ғʀᴇᴇ sᴜʙsᴄʀɪᴘᴛɪᴏɴ 💰', callback_data="subscription")
+        ],[
+            InlineKeyboardButton("❌ ᴄʟᴏꜱᴇ ❌", callback_data="close_data")
     ]]
     await message.reply_photo(photo="https://graph.org/file/f96562518138b9132abf8.jpg", caption=script.PREMIUM_TXT.format(message.from_user.mention), reply_markup=InlineKeyboardMarkup(btn))
 
