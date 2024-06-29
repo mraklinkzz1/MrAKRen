19588ff03fccd1350d01a7b491e0355da19c3201
MrAK Techz <l20cs001@vvcoe.org>
2024-06-29 20:23:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -46,8 +46,8 @@ CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHAN
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '').split()]
-auth_channel = environ.get('AUTH_CHANNEL', '')
-auth_channel_2 = environ.get('AUTH_CHANNEL', '') #Channel / Group Id for force sub ( make sure bot is admin )
+auth_channel = environ.get('AUTH_CHANNEL', '-1002145921498')
+auth_channel_2 = environ.get('AUTH_CHANNEL', '-1002044250842') #Channel / Group Id for force sub ( make sure bot is admin )
  #Channel / Group Id for force sub ( make sure bot is admin )
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
