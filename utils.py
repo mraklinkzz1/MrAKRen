ba496bf3526e0c9b188bb767c43b16f4936eae6e
MrAK Techz <l20cs001@vvcoe.org>
2024-06-29 20:20:55 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,9 +1,9 @@
 import logging
 from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait, UserIsBlocked, PeerIdInvalid
-from info import AUTH_CHANNEL, LONG_IMDB_DESCRIPTION, MAX_LIST_ELM, SHORTLINK_URL, SHORTLINK_API, IS_SHORTLINK, LOG_CHANNEL, TUTORIAL, GRP_LNK, CHNL_LNK, CUSTOM_FILE_CAPTION
+from info import AUTH_CHANNEL, LONG_IMDB_DESCRIPTION, MAX_LIST_ELM, SHORTLINK_URL, SHORTLINK_API, IS_SHORTLINK, LOG_CHANNEL, TUTORIAL, GRP_LNK, CHNL_LNK, CUSTOM_FILE_CAPTION, AUTH_CHANNEL_2
 from imdb import Cinemagoer 
 import asyncio
-from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, ChatAdminRequired
 from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid
 from pyrogram import enums
 from typing import Union
@@ -54,20 +54,29 @@ class temp(object):
     SETTINGS = {}
     IMDB_CAP = {}
 
-async def is_req_subscribed(bot, query):
-    if await db.find_join_req(query.from_user.id):
-        return True
-    try:
-        user = await bot.get_chat_member(AUTH_CHANNEL, query.from_user.id)
-    except UserNotParticipant:
-        pass
-    except Exception as e:
-        logger.exception(e)
-    else:
-        if user.status != enums.ChatMemberStatus.BANNED:
-            return True
+async def is_req_subscribed(bot, query=None, userid=None):
+    subscribed_to_all = True
 
-    return False
+    for channel_id in [AUTH_CHANNEL, AUTH_CHANNEL_2]:
+        try:
+            if userid is None and query is not None:
+                user = await bot.get_chat_member(channel_id, query.from_user.id)
+            else:
+                user = await bot.get_chat_member(channel_id, int(userid))
+        except ChatAdminRequired:
+            subscribed_to_all = False
+            try:
+                invite_link = await bot.export_chat_invite_link(channel_id)
+                await bot.send_message(chat_id=userid, text=invite_link)
+            except Exception:
+                pass
+        except Exception:
+            subscribed_to_all = False
+        else:
+            if user.status == "kicked":
+                subscribed_to_all = False
+
+    return subscribed_to_all
 
 async def get_poster(query, bulk=False, id=False, file=None):
     if not id:
